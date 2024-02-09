493a5e7daff863eaaae870047a394fca9f00a3b6
Ajay <chavdaajay129@gmail.com>
2024-02-09 11:01:15 +0530

    cord added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors') // Import the cors package
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
@@ -10,6 +11,7 @@ const app = express()
 
 // middleware
 app.use(express.json())
+app.use(cors()) // Use cors middleware to enable CORS
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
